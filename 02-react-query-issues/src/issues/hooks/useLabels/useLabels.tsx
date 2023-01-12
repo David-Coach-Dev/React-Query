d9d928644a111f32b54e4f16b3350fdbe54bf49d
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-12 16:28:40 -0500

    act arch

--- 02-react-query-issues/src/issues/hooks/useLabels/useLabels.tsx
+++ 02-react-query-issues/src/issues/hooks/useLabels/useLabels.tsx
@@ -2,8 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getLabels } from "../../../api/gets/Labels/getLabels";
 
 export const useLabels = () => {
-  const labelsQuery = useQuery(["labels"], getLabels, {
-    refetchOnWindowFocus: false,
-  });
+  const labelsQuery = useQuery(
+    ["labels"],
+    getLabels,
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+    }
+  );
   return labelsQuery;
 };
